91892850b28ff623ffbed300f120cb710c25d477
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-05-08 19:44:24 +0530

    first django project

---
+++ polls/models.py
@@ -0,0 +1,24 @@
+from secrets import choice
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+
+class Question(models.Model):
+    #...
+    def __str__(self):
+        return self.question_txt
+        #...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    question_txt=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+
+class Choice(models.Model):
+     #...
+    def __str__(self):
+        return self.choice_text
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    choice_text=models.CharField(max_length=200)
+    votes=models.IntegerField(default=0)

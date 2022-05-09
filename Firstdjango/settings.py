c36dc845bdd0cfc7f03ea835ac51a0d61e5275b0
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-05-09 09:11:05 +0530

    info app

--- Firstdjango/settings.py
+++ Firstdjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'infoapp.apps.InfoappConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- infoapp/models.py
+++ infoapp/models.py
@@ -3,9 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Info(models.Model):
+    #...    
+    def __str__(self):  
+        return self.info_txt
     info_txt=models.CharField(max_length=300)
 
 class Subinfo(models.Model):
+    #...
+    def __str__(self):
+        return self.subinfo_txt
     info=models.ForeignKey(Info,on_delete=models.CASCADE)
-    
+
     subinfo_txt=models.CharField(max_length=300)
